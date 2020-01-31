feee5ad29f8fed6eb268fb2f3c8159b61d6ad368
Claudia Silva <csilva@allin.com.br>
2020-01-31 12:09:20 -0300

    update files

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -4,14 +4,13 @@ public class Principal {
 
     public static void main(String args[]) {
 
-
-        System.out.println("INICIANDO >>>>>>>>");
-
         Main principal  = new Main();
+//        System.out.println(principal.q1());
+//        System.out.println(principal.q2());
+//        System.out.println(principal.q3());
+        System.out.println(principal.q4());
+//        System.out.println(principal.q5());
+//        System.out.println(principal.q6());
 
-        System.out.println(principal.q1());
-        System.out.println(principal.q2());
-        System.out.println(principal.q3());
-//        System.out.println(principal.q4());
     }
 }
--- src/main/java/challenge/Main.java
+++ src/main/java/challenge/Main.java
@@ -1,7 +1,6 @@
 package challenge;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -9,6 +8,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import static java.util.stream.Collectors.toList;
 
 public class Main {
 
@@ -17,8 +17,7 @@ public class Main {
 	// Quantas nacionalidades (coluna `nationality`) diferentes existem no arquivo?
 	public int q1() {
 
-		resultPlayer = this.csvRead();
-		return (int) resultPlayer.stream()
+		return (int) this.csvRead().stream()
 				.map(Player::getNationality)
 				.distinct()
 				.count();
@@ -28,8 +27,7 @@ public class Main {
 	// Obs: Existem jogadores sem clube.
 	public int q2() {
 
-		resultPlayer = this.csvRead();
-		return (int) resultPlayer.stream()
+		return (int) this.csvRead().stream()
 				.filter(n -> !n.getClub().isEmpty())
 				.map(Player::getClub)
 				.distinct()
@@ -39,8 +37,7 @@ public class Main {
 	// Liste o nome completo (coluna `full_name`) dos 20 primeiros jogadores.
 	public List<String> q3() {
 
-		resultPlayer = this.csvRead();
-		return resultPlayer.stream()
+		return this.csvRead().stream()
 				.map(Player::getFullName)
 				.limit(20)
 				.collect(Collectors.toList());
@@ -50,29 +47,40 @@ public class Main {
 	// (utilize as colunas `full_name` e `eur_release_clause`)
 	public List<String> q4() {
 
-//		resultPlayer = this.csvRead();
-//		return resultPlayer.stream()
-//				.filter(p -> !p.getEurReleaseClause().isEmpty())
-//				.sorted(Comparator.rever)
-//				.map(Player::getFullName)
-//				.limit(10)
-//				.collect(Collectors.toList());
-		return null;
+		return this.csvRead().stream()
+				.filter(p -> !p.getEurReleaseClause().isEmpty())
+				.sorted(Comparator.comparing(Player::getEurReleaseClause))
+				.map(Player::getFullName)
+				.limit(10)
+				.collect(Collectors.toList());
 	}
 
 	// Quem são os 10 jogadores mais velhos (use como critério de desempate o campo `eur_wage`)?
 	// (utilize as colunas `full_name` e `birth_date`)
 	public List<String> q5() {
-		return null;
+
+		return this.csvRead().stream()
+				.sorted(Comparator.comparing(Player::getBirthDate)
+					.thenComparing(Player::getEurWage))
+				.map(Player::getFullName)
+				.limit(10)
+				.collect(Collectors.toList());
 	}
 
 	// Conte quantos jogadores existem por idade. Para isso, construa um mapa onde as chaves são as idades e os valores a contagem.
 	// (utilize a coluna `age`)
 	public Map<Integer, Integer> q6() {
 
-//		resultPlayer = this.csvRead();
-		return null;
+		Map<Integer, Long> ageMap = this.csvRead().stream()
+				.collect(Collectors.groupingBy((Player p) -> Integer.parseInt(p.getAge()),
+						Collectors.counting()));
 
+		Map<Integer, Integer>  finalResult = ageMap
+				.entrySet()
+				.stream()
+				.collect(Collectors.toMap(Map.Entry::getKey, p -> p.getValue().intValue()));
+
+		return finalResult;
 	}
 
 	private static List<Player> csvRead() {
@@ -86,7 +94,7 @@ public class Main {
 			BufferedReader bufferedReader = new BufferedReader((new FileReader("/home/claudiasilva/codenation/java-3/src/main/resources/data.csv")));
 			bufferedReader.readLine();
 
-			while( (lineFile = bufferedReader.readLine()) != null) {
+			while( (lineFile = bufferedReader.readLine()) != null ) {
 
 				String[] player = lineFile.split(lineSplit);
 
@@ -107,11 +115,9 @@ public class Main {
 			}
 
 		} catch(IOException e) {
-
 			e.getMessage();
 		}
 
 		return listPlayer;
 	}
-
 }
